1c046a1f3a23c66a53b5702924d2988e5b58ecc1
w0214 <2502003387@qq.com>
2023-02-14 20:19:06 +0800

    创建路由

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/users/:id', to: 'users#show'
-  post '/users/', to: 'users#create'
+  namespace :api do
+    namespace :v1 do
+      resources :validation_codes,only:[:create]
+      resource :session,only:[:create,:destroy]
+      resource :me,only:[:show]
+      resources :items
+      resources :tags
+    end
+  end
 end
